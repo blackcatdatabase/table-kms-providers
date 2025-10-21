9db5afdce708451d7b7492072ba79ecf43f1e1e0
Tolben <vitek.pasek@gmail.com>
2025-10-21 02:36:35 +0200

    chore(schema): split from umbrella

---
+++ schema/001_table.sql
@@ -0,0 +1,11 @@
+-- Auto-generated from schema-map.psd1 on 2025-10-21T02:32:05
+-- table: kms_providers
+CREATE TABLE IF NOT EXISTS kms_providers (
+  id BIGINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(100) NOT NULL,
+  provider ENUM(''gcp'',''aws'',''azure'',''vault'') NOT NULL,
+  location VARCHAR(100) NULL,
+  project_tenant VARCHAR(150) NULL,
+  created_at DATETIME(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6),
+  is_enabled BOOLEAN NOT NULL DEFAULT TRUE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
