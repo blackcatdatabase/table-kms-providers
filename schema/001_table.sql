a255fe6a39b47c58d8af91475f0c22c49e6671d1
Tolben <vitek.pasek@gmail.com>
2025-10-22 20:24:30 +0200

    chore(schema): add/update generated schema + README (umbrella sync)

--- schema/001_table.sql
+++ schema/001_table.sql
@@ -3,7 +3,7 @@
 CREATE TABLE IF NOT EXISTS kms_providers (
   id BIGINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
   name VARCHAR(100) NOT NULL,
-  provider ENUM(''gcp'',''aws'',''azure'',''vault'') NOT NULL,
+  provider ENUM('gcp','aws','azure','vault') NOT NULL,
   location VARCHAR(100) NULL,
   project_tenant VARCHAR(150) NULL,
   created_at DATETIME(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6),
